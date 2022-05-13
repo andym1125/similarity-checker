a56dd74e1c99db0a613cf861d9f877622f7f60a2
Andy M <andym1125@gmail.com>
2022-05-13 18:18:32 -0500

    Frontend calcs to determine what to highlight. Does not talk to backend yet.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+function App() 
+{
+  const [taContent, setTaContent] = useState();
+  const [fmtContent, setFmtContent] = useState();
+
+  function checkPlagarism()
+  {
+
+    alert("test")
+
+  }
+
+  function onTaChange(e)
+  {
+    setTaContent(e.target.value)
+    processInput(e.target.value)
+  }
+
+  let processInput = debounce(content =>
+  {
+    let substrs = [
+      {
+        start: 1,
+        end: 3,
+        source: 1
+      },
+      {
+        start: 4,
+        end: 5,
+        source: 2
+      }
+    ]
+
+    fetch('https://dummyjson.com/products')
+      .then(response => substrs)
+      .then(data => 
+      {
+        /* Substrings should be wrapped in HTML */
+        let els = []
+        let lastSlice = 0
+        //PRECONDITION: None of the substrings overlap
+        for(let i = 0; i < content.length; i++)
+        {
+          //Find one with start at current index
+          for(let j = 0; j < data.length; j++)
+          {
+            if(data[j].start === i)
+            {
+              els.push(content.slice(lastSlice, data[j].start))
+              els.push(<span class="highlight">{content.slice(data[j].start, data[j].end)}</span>)
+              lastSlice = data[j].end;
+              i = data[j].end - 1;
+              break;
+            }
+          }
+        }
+        els.push(content.slice(lastSlice))
+
+        console.log(els)
+        setFmtContent(els)
+      });
+  }, 1000);
+
   return (
     <div className="App">
       <h1>Enter text to check for plagarism:</h1>
-      <form method="POST" action="/">
-        <input type="text" name="txt"></input>
-        <br></br>
-        <input type="submit" value="Check for Plagarism"></input>
-      </form>
+      <textarea id="content" name="content" onChange={onTaChange} value={taContent}></textarea>
+      <br></br>
+      <button onClick={checkPlagarism}>Check for Plagarism</button>
+      <p>{fmtContent}</p>
     </div>
   );
+}
+
+/* Source: Modified from Web Dev Simplified
+ * https://www.youtube.com/watch?v=cjIswDCKgu0
+ */
+let timeout;
+function debounce(callback, delay = 500)
+{
+  return (...args) =>
+  {
+    clearTimeout(timeout);
+    timeout = setTimeout(() =>
+    {
+      callback(...args)
+    }, delay)
+  }
 }
 
 export default App;
